d774478b3d009b22637667c715ce95e3907213f7
nicolasdomashke <nicolasdomashke@gmail.com>
2024-09-04 19:57:45 +0300

    unification

--- server.py
+++ server.py
@@ -2,7 +2,13 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+@app.get("/")
+async def read_root():
+    print("Root open")
+    return {"message": "Hello, World!"}
+
 @app.post("/data")
 async def receive_data(data):
     # Process data here
+    print("Data post")
     return {"status": "success", "data": data.dict()}
