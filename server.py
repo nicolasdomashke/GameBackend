0dbab5fc8c644d23e80bc58fa9084bc2715a9029
nicolasdomashke <nicolasdomashke@gmail.com>
2024-09-04 19:54:44 +0300

    add root

--- server.py
+++ server.py
@@ -2,10 +2,6 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-async def read_root():
-    return {"message": "Hello, World!"}
-
 @app.post("/data")
 async def receive_data(data):
     # Process data here
