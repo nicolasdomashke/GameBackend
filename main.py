5ef338cf09e413714221d5bd2ba606b3659f8a61
nicolasdomashke <nicolasdomashke@gmail.com>
2024-09-04 19:56:50 +0300

    add prints

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ app = FastAPI()
 
 @app.get("/")
 async def read_root():
+    print("Root open")
     return {"message": "Hello, World!"}
 
 @app.post("/data")
 async def receive_data(data):
     # Process data here
+    print("Data post")
     return {"status": "success", "data": data.dict()}
