e90746c1ef05e801438a49d5da63717e8d5fdc1b
nicolasdomashke <nicolasdomashke@gmail.com>
2024-09-05 18:12:54 +0300

    basics complete

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel
 from sqlalchemy import create_engine, Column, Integer, String, Time
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from datetime import time
 
 Base = declarative_base()
 engine = create_engine('sqlite:///database.sqlite3')
@@ -17,6 +18,11 @@ class User_data(BaseModel):
     login: str
     password: str
 
+class Time_data(BaseModel):
+    login: str
+    level: int = 0
+    time0: int
+
 class User(Base):
     __tablename__ = 'users'
 
@@ -29,6 +35,22 @@ class User(Base):
     time4 = Column(Time)
     time5 = Column(Time)
     timet = Column(Time)
+
+def float_to_time(santiseconds):
+    if santiseconds >= 360000:
+        santiseconds = 359999
+    minutes, santiseconds = divmod(santiseconds, 6000)
+    seconds, santiseconds = divmod(santiseconds, 100)
+    return time(minute=minutes, second=seconds, microsecond=santiseconds * 10000)
+
+def time_reformat(t: time):
+    return f"{t.minute}:{t.second}:{t.microsecond // 10000}"
+
+@app.get("/leaderboard/{level}")
+async def get_leaderboard(level: int):
+    query = session.query(User.login, getattr(User, f"time{level}")).all()
+    result = {row.login: time_reformat(row._mapping[getattr(User, f"time{level}")]) for row in query}
+    return result
 
 @app.post("/register")
 async def new_user(data: User_data):
@@ -45,6 +67,16 @@ async def new_user(data: User_data):
 async def login(data: User_data):
     query = session.query(User).filter(User.login == data.login, User.password == data.password)
     if query.all():
+        return {"status": "success"}
+    else:
+        return {"status": "error"}
+    
+@app.post("/insert")
+async def insert_time(data: Time_data):
+    query = session.query(User).filter(User.login == data.login).first()
+    if query:
+        setattr(query, f"time{data.level}", float_to_time(data.time0))
+        session.commit()
         return {"status": "success"}
     else:
         return {"status": "error"}
